ee5f3dc84891e0e23b13503c942942d784c2388f
Alam-2006 <alamfzdk7@gmail.com>
2025-11-25 12:09:10 +0530

    Clarify comparison vs equality operators in JS
    Added comments explaining the behavior of comparison and equality operators in JavaScript.

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -7,6 +7,7 @@
 
 // console.log("2" > 1);
 // console.log("02" > 1);
+// both comparison and equality operator work differently in java script ...the comparision operator convert NULL to number that why >=0 is true while the equality operator gives false 
 
 console.log(null > 0);
 console.log(null == 0);
